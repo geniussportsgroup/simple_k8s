ccfb1b81cd165abc129f4eb636fe6068bd026f1a
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2023-01-09 16:15:33 -0500

    Added liveness check support through grpc-health protocol

---
+++ health_check.go
@@ -0,0 +1,35 @@
+package simple_k8s
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"log"
+	"net"
+)
+
+const HealthPort = 9666
+const CheckName = "alive"
+
+var healthCheckServer *health.Server
+
+func EnableLivelinessCheck() {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", HealthPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	healthCheckServer = health.NewServer()
+	healthCheckServer.SetServingStatus(CheckName, healthpb.HealthCheckResponse_SERVING)
+	healthgrpc.RegisterHealthServer(s, healthCheckServer)
+	s.Serve(lis)
+}
+
+func DisableLivelinessCheck() {
+
+	healthCheckServer.SetServingStatus(CheckName, healthpb.HealthCheckResponse_NOT_SERVING)
+}
